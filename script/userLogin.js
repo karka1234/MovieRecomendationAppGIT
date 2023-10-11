d2471e4bb631819150d1675357ede1d833835193
Andzej Z <Andzej.zdanovski@gmail.com>
2023-10-11 19:14:33 +0300

    Ceater User profile

--- script/userLogin.js
+++ script/userLogin.js
@@ -11,6 +11,7 @@ window.addEventListener("load",()=> {
         if(existingUser)
         
         alert("Welcome "+ Userinfo)
+        window.location.href = "pages/UserProfile.html";
     })
 
     
