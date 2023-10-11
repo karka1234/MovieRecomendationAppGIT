2376d78c86f3114c7901c6c4352168922304e06b
Andzej Z <Andzej.zdanovski@gmail.com>
2023-10-11 18:11:16 +0300

    Added User Login Page

--- script/user.js
+++ script/user.js
@@ -1,9 +1,10 @@
 window.addEventListener("load",()=> {
-    const form= document.querySelector("form")
-    form.addEventListener("submit", ()=>{
+    const form= document.querySelector("#loginpage")
+    form.addEventListener("submit",()=>{
         const firstName =document.querySelector("#fname").value;
         const lastName =document.querySelector("#lname").value;
         const email = document.querySelector("#email").value;
+        const password = document.querySelector("#pwd").value;
 
         const storedUsers = localStorage.getItem("users") || "[]";
         const users = JSON.parse(storedUsers);
@@ -13,6 +14,7 @@ window.addEventListener("load",()=> {
       firstName: firstName,
       lastName: lastName,
       email: email,
+      password:password,
       movieList:[],
     }
     users.push(user);
---
+++ script/userLogin.js
@@ -0,0 +1,18 @@
+window.addEventListener("load",()=> {
+    const form= document.querySelector("form")
+    form.addEventListener("submit", ()=>{
+        const password = document.querySelector("#password").value;
+        const email = document.querySelector("#email").value;
+        const userList = localStorage.getItem("users") || "[]";
+        const users = JSON.parse(userList);
+        const existingUser = users.find((user) => user.email === email && user.password === password);
+        const Userinfo=JSON.stringify(existingUser.firstName);
+        
+        if(existingUser)
+        
+        alert("Welcome "+ Userinfo)
+    })
+
+    
+
+})
